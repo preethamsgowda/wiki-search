1d6e3ed60131b41f05cd58e557ad7f0910ab06f4
DESKTOP-TMNJGQK\Preetham <pritam.gowda007@gmail.com>
2021-03-07 19:06:34 -0500

    Updates

---
+++ static/js/main.c29142c0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpwiki-search"]=this["webpackJsonpwiki-search"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);n(1);var s=n(3),c=n.n(s),i=n(0),r=function(){return Object(i.jsx)("div",{children:"Test, ayyo!"})};c.a.render(Object(i.jsx)(r,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.c29142c0.chunk.js.map
